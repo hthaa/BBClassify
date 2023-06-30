93b1ef5039ecd27f6a4a234a57d42a86a0b7f18e
hthaa <h.t.haakstad@cemo.uio.no>
2023-06-30 11:54:11 +0200

    Minor

--- BB-Classify.py
+++ BB-Classify.py
@@ -259,7 +259,8 @@ def betaparameters(x: list, n: int, k: float, model: int = 4, l: float = 0, u: f
 #       Brennan approach.
 # output = list of strings which state what analyses to do and include in the 
 #       output.
-def cac(x, reliability: float, min: float, max: float, cut: float, model: int = 4, l: float = 0, u: float = 1, failsafe: bool = False, method: str = "ll", output: list[str] = ["parameters", "accuracy", "consistency"]):
+# print_output = Whether to print the output to the consoel as it is estimated.
+def cac(x, reliability: float, min: float, max: float, cut: float, model: int = 4, l: float = 0, u: float = 1, failsafe: bool = False, method: str = "ll", output: list[str] = ["parameters", "accuracy", "consistency"], print_output = True):
     out = {}
     cut = [min] + cut + [max]
     tcut = list(cut)
@@ -293,6 +294,10 @@ def cac(x, reliability: float, min: float, max: float, cut: float, model: int = 
             pars["lords_k"] = K
     if "parameters" in output:
         out["parameters"] = pars
+        if print_output:
+            print("Model parameter estimates:")
+            print(out["parameters"])
+            print("")
 
     if "accuracy" in output:
         print("Estimating accuracy...\n")
@@ -313,6 +318,13 @@ def cac(x, reliability: float, min: float, max: float, cut: float, model: int = 
         accuracy = sum(accuracy)
         out["Confusion matrix"] = pd.DataFrame(confusionmatrix)
         out["Overall accuracy"] = accuracy
+        if print_output:
+            print("Confusion matrix:")
+            print(out["Confusion matrix"])
+            print("")
+            print("Overall accuracy:")
+            print(out["Overall accuracy"])
+            print("")
     
     if "consistency" in output:
         print("Estimating consistency...\n")
@@ -350,141 +362,31 @@ def cac(x, reliability: float, min: float, max: float, cut: float, model: int = 
             consistency = sum(consistency)
         out["Consistency matrix"] = pd.DataFrame(consistencymatrix)
         out["Overall consistency"] = consistency
-    return out
-
-def cac2(x, reliability: float, min: float, max: float, cut: float, model: int = 4, l: float = 0, u: float = 1, failsafe: bool = False, method: str = "ll", output: list[str] = ["parameters", "accuracy", "consistency"]):
-    out = {}
-    cut = [min] + cut + [max]
-    tcut = list(cut)
-    for i in range(len(cut)):
-        tcut[i] = (tcut[i] - min) / (max - min)
-    if isinstance(x, dict):
-        pars = x
-        if method == "ll":
-            N = pars["etl"]
-        else:
-            N = pars["atl"]
-    else:
-        if method == "ll":
-            Nnotrounded = etl(stats.mean(x), stats.variance(x), reliability, min, max)
-            N = round(Nnotrounded)
-            pars = betaparameters(x, N, 0, model, l, u)
-            if (failsafe == True and model == 4) and (l < 0 or u > 1):
-                pars = betaparameters(x, N, 0, 2, l, u)
-            pars["etl"] = Nnotrounded
-            pars["etl rounded"] = N
-            pars["lords_k"] = 0
-            for i in range(len(cut)):
-                cut[i] = tcut[i] * N
-                cut[i] = round(cut[i])
-        else:
-            N = max 
-            K = k(stats.mean(x), stats.variance(x), reliability, N)
-            pars = betaparameters(x, N, K, model, l, u)
-            if (failsafe == True and model == 4) and (l < 0 or u > 1):
-                pars = betaparameters(x, max, N, 2, l, u)
-            pars["lords_k"] = K
-    if "parameters" in output:
-        out["parameters"] = pars
-
-    if "accuracy" in output:
-        print("Estimating accuracy...\n")
-        confmat = np.zeros((N + 1, len(cut) - 1))
-        for i in range(len(cut) - 1):
-            for j in range(N + 1):
-                confmat[j, i] = bbintegrate1(pars["alpha"], pars["beta"], pars["l"], pars["u"], N, j, pars["lords_k"], tcut[i], tcut[i + 1], method)[0]
-        confusionmatrix = np.zeros((len(cut) - 1, len(cut) - 1))
-        for i in range(len(cut) - 1):
-            for j in range(len(cut) - 1):
-                if i != len(cut) - 2:
-                    confusionmatrix[i, j] = sum(confmat[cut[i]:cut[i + 1], j])
-                else:
-                    confusionmatrix[i, j] = sum(confmat[cut[i]:, j])
-        accuracy = []
-        for i in range(len(cut) - 1):
-            accuracy = accuracy + [confusionmatrix[i, i]]
-        accuracy = sum(accuracy)
-        out["Confusion matrix"] = pd.DataFrame(confusionmatrix)
-        out["Overall accuracy"] = accuracy
-    
-    if "consistency" in output:
-        print("Estimating consistency...\n")
-        consmat = np.zeros((N + 1, N + 1))
-        for i in range(N + 1):
-            for j in range(N + 1):
-                consmat[i, j] = bbintegrate2(pars["alpha"], pars["beta"], pars["l"], pars["u"], N, i, j, pars["lords_k"], 0, 1, method)[0]
-        consistencymatrix = np.zeros((len(cut) - 1, len(cut) - 1))
-        for i in range(len(cut) - 1):
-            for j in range(len(cut) - 1):
-                if i == 0 and j == 0:
-                    consistencymatrix[i, j] = sum(sum(consmat[0:cut[i + 1], 0:cut[j + 1]]))
-                if i == 0 and (j != 0 and j != len(cut) - 2):
-                    consistencymatrix[i, j] = sum(sum(consmat[0:cut[i + 1], cut[j]:cut[j + 1]]))
-                if i == 0  and j == len(cut) - 2:
-                    consistencymatrix[i, j] = sum(sum(consmat[0:cut[i + 1], cut[j]:cut[j + 1] + 1]))
-                if (i != 0 and i != len(cut) - 2) and j == 0:
-                    consistencymatrix[i, j] = sum(sum(consmat[cut[i]:cut[i + 1], 0:cut[j + 1]]))
-                if (i != 0 and i != len(cut) - 2) and (j != 0 and j != len(cut) - 2):
-                    consistencymatrix[i, j] = sum(sum(consmat[cut[i]:cut[i + 1], cut[j]:cut[j + 1]]))
-                if (i != 0 and i != len(cut) - 2) and j == len(cut) - 2:
-                    consistencymatrix[i, j] = sum(sum(consmat[cut[i]:cut[i + 1], cut[j]:cut[j + 1] + 1]))
-                if i == len(cut) - 2 and j == 0:
-                    consistencymatrix[i, j] = sum(sum(consmat[cut[i]:cut[i + 1] + 1, 0:cut[j + 1]]))
-                if i == len(cut) - 2 and (j != 0 and j != len(cut) - 2):
-                    consistencymatrix[i, j] = sum(sum(consmat[cut[i]:cut[i + 1] + 1, cut[j]:cut[j + 1]]))
-                if i == len(cut) - 2 and j == len(cut) - 2:
-                    consistencymatrix[i, j] = sum(sum(consmat[cut[i]:cut[i + 1] + 1, cut[j]:cut[j + 1] + 1]))
-            consistency = []
-            for i in range(len(cut) - 1):
-                consistency = consistency + [consistencymatrix[i, i]]
-            consistency = sum(consistency)
-        out["Consistency matrix"] = pd.DataFrame(consistencymatrix)
-        out["Overall consistency"] = consistency
-    return out
-
-
+        if print_output:
+            print("Consistency matrix:")
+            print(out["Consistency matrix"])
+            print("")
+            print("Overall consistency:")
+            print(out["Overall consistency"])
+        return out
 
 # Setting the seed
-np.random.seed(1234)
+np.random.seed(123456)
 
 # Define the parameters for the beta distribution
 a, b = 6, 4
 # The first two parameters are for the location and scale parameters respectively
-p_success = rbeta4p(1000, 6, 4, .15, .85)
+p_success = rbeta4p(100000, 6, 4, .15, .85)
 
 # Preallocate a matrix of zeros with 1000 rows and 20 columns
-rawdata = np.zeros((1000, 100))
+rawdata = np.zeros((100000, 100))
 
 # Loop over the columns
-for i in range(1000):
+for i in range(100000):
     for j in range(100):
     # For each column, generate binomially distributed data
         rawdata[i, j] = np.random.binomial(1, p_success[i], 1)
 sumscores = np.sum(rawdata, axis = 1)
+meanscores = np.mean(rawdata, axis = 1)
 
-from time import time
-start = time()
-output1 = cac(sumscores, cronbachs_alpha(rawdata), 0, 100, [50, 75], method="hb")
-end = time()
-print(end - start)
-print(output1["Consistency matrix"])
-#for i in output1:
-#    print("\n")
-#    print(i)
-#    print(output1[i])
-print("No lower tri")
-#start = time()
-##output2 = cac2(sumscores, cronbachs_alpha(rawdata), 0, 20, [8, 12], method="hb")
-#end = time()
-#print(end - start)
-#print(output2["Consistency matrix"])
-
-#for i in output2:
-#    print("\n")
-#    print(i)
-#    print(output2[i])
-#print("With lower tri")
-
-
-
-
+output = cac(sumscores, cronbachs_alpha(rawdata), 0, 100, [50, 75])
